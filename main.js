eec1237a9d7d1b79fe2d75d1b3f1c2666ec9828c
Chris Lewis <cl_myles@hotmail.com>
2018-07-20 00:03:02 +1000

    Creation of action list HTML and various styling improvements

--- main.js
+++ main.js
@@ -4,6 +4,14 @@
 //([~+-]|-\/\+) .*?^ {0,2}(?=[~+-]|-\/\+|Plan)
 //^\s*(.*):\s*"(.*)" => (.*)$
 
+function handleSummaryToggle(element)
+{
+    return function () 
+    {
+        accordion(element);
+    };
+}
+
 function accordion(element)
 {
     var changes = element.parentElement.getElementsByClassName('changes');
@@ -29,35 +37,37 @@ function parse()
 {
     var terraformPlan = document.getElementById("terraform-plan").value;
     
-    var warningRegex = new RegExp('Warning: (.*)', 'gm');
+    var warningRegex = new RegExp('Warning: (.*:)(.*)', 'gm');
     var warning;
     var warnings = [];
 
     do
     {
         warning = warningRegex.exec(terraformPlan);
-        if (warning) warnings.push(warning[1]);
+        if (warning) warnings.push({ id: warning[1], detail: warning[2] });
     } while (warning);
 
-    console.log(warnings);
+    // console.log(warnings);
 
     var beginActionRegex = new RegExp('Terraform will perform the following actions:', 'gm');
     var begin = beginActionRegex.exec(terraformPlan);
     var actions = terraformPlan.substring(begin.index + 45);
 
     //TODO: fix chopping off the '-/' in '-/+' changes
-    var changeRegex = new RegExp('([~+-]|-\/\+) .*?^ {0,2}(?=-\/\+|[~+-]|Plan)', 'gms');
+    var changeRegex = new RegExp('([~+-]|-\/\+) [\\S\\s]*?\\s(?=-\/\+|[~+-]|Plan:)', 'gm');
     var change;
     var changes = [];
 
+    // console.log(actions);
     do
     {
         change = changeRegex.exec(actions);
+        // console.log(change);
         if (change) changes.push(change[0]);
     } while(change);
     
     // console.log(actions.match(new RegExp('([~+-]|-\/\+) .*?(?=^ {0,2}[~+-]|-\/\+|Plan)', 'gms')));
-    console.log(changes);
+    // console.log(changes);
 
     var summary = { warnings: warnings, actions: [] };
     for (var i = 0; i < changes.length; i++)
@@ -89,7 +99,7 @@ function parse()
             id[2] = id[2].replace(' (new resource required)', '');
         }
 
-        summary.actions.push({ id: id[2], type: type, diffs: diffs });
+        summary.actions.push({ id: id[2], type: type, changes: diffs });
     }
 
     console.log(summary);
@@ -109,14 +119,78 @@ function output(plan)
 
             var id = document.createElement('span');
             id.className = 'id';
-            id.innerText = plan.warnings[i];
+            id.innerText = plan.warnings[i].id;
 
-            // var detail = document.createElement('span');
+            var detail = document.createElement('span');
+            detail.innerText = plan.warnings[i].detail;
             
             var listElement = document.createElement('li');
             listElement.appendChild(badge);
             listElement.appendChild(id);
+            listElement.appendChild(detail);
             warningList.appendChild(listElement);
+        }
+    }
+
+    if (plan.actions)
+    {
+        var actionList = document.getElementById('actions');
+        for (var i = 0; i < plan.actions.length; i++)
+        {
+            var badge = document.createElement('span');
+            badge.className = 'badge';
+            badge.innerText = plan.actions[i].type;
+
+            var id = document.createElement('span');
+            id.className = 'id';
+            id.innerText = plan.actions[i].id;
+
+            var changeCount = document.createElement('span');
+            changeCount.className = 'change-count';
+            if (plan.actions[i].changes)
+            {
+                changeCount.innerText = ' ' + plan.actions[i].changes.length + ' change';
+                if (plan.actions[i].changes.length > 1) changeCount.innerText += 's';
+            }
+
+            var summary = document.createElement('div');
+            summary.className = 'summary';
+            summary.onclick = handleSummaryToggle(summary);
+            summary.appendChild(badge);
+            summary.appendChild(id);
+            summary.appendChild(changeCount);
+
+            var changesTable = document.createElement('table');
+            for (var c = 0; c < plan.actions[i].changes.length; c++)
+            {
+                var property = document.createElement('td');
+                property.className = 'property';
+                property.innerText = plan.actions[i].changes[c].property;
+
+                var oldValue = document.createElement('td');
+                oldValue.className = 'old-value';
+                oldValue.innerHTML = plan.actions[i].changes[c].old;
+
+                var newValue = document.createElement('td');
+                newValue.className = 'new-value';
+                newValue.innerText = plan.actions[i].changes[c].new;
+
+                var row = document.createElement('tr');
+                row.appendChild(property);
+                row.appendChild(oldValue);
+                row.appendChild(newValue);
+                changesTable.appendChild(row);
+            }
+            
+            var changes = document.createElement('div');
+            changes.className = 'changes collapsed';
+            changes.appendChild(changesTable);
+
+            var listElement = document.createElement('li');
+            listElement.className = plan.actions[i].type;
+            listElement.appendChild(summary);
+            listElement.appendChild(changes);
+            actionList.appendChild(listElement);
         }
     }
 }
