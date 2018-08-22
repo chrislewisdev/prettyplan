a29db44e080bd02a79e95b777195a6bded07e9cc
chrisl <chrisl@pageuppeople.com>
2018-08-22 13:06:53 +1000

    Refactor the output() function into several smaller functions

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function run()
     var terraformPlan = document.getElementById("terraform-plan").value;
     var plan = parse(terraformPlan);
     console.log(plan);
-    output(plan);
+    render(plan);
 }
 
 function parse(terraformPlan)
@@ -73,7 +73,7 @@ function extractChangeSummary(terraformPlan) {
 }
 
 function extractIndividualChanges(changeSummary) {
-    //TODO: Fix thee '-/' in '-/+' getting chopped off
+    //TODO: Fix the '-/' in '-/+' getting chopped off
     var changeRegex = new RegExp('([~+-]|-\/\+|<=) [\\S\\s]*?\\s(?=-\/\+|[~+-]|<=|Plan:)', 'gm');
     var change;
     var changes = [];
@@ -155,118 +155,144 @@ function parseNewAndOldValueDiffs(change)
     return diffs;
 }
 
-function output(plan)
+function render(plan)
 {
     if (plan.warnings)
     {
         var warningList = document.getElementById('warnings');
         for (var i = 0; i < plan.warnings.length; i++)
         {
-            var badge = document.createElement('span');
-            badge.className = 'badge';
-            badge.innerText = 'warning';
-
-            var id = document.createElement('span');
-            id.className = 'id';
-            id.innerText = plan.warnings[i].id;
-
-            var detail = document.createElement('span');
-            detail.innerText = plan.warnings[i].detail;
-            
-            var listElement = document.createElement('li');
-            listElement.appendChild(badge);
-            listElement.appendChild(id);
-            listElement.appendChild(detail);
-            warningList.appendChild(listElement);
+            var warning = createWarningElement(plan.warnings[i], warningList);
+            warningList.appendChild(warning);
         }
     }
-
+    
     if (plan.actions)
     {
         var actionList = document.getElementById('actions');
         for (var i = 0; i < plan.actions.length; i++)
         {
-            var badge = document.createElement('span');
-            badge.className = 'badge';
-            badge.innerText = plan.actions[i].type;
-
-            var id = document.createElement('span');
-            id.className = 'id';
-            id.innerText = plan.actions[i].id;
-
-            var changeCount = document.createElement('span');
-            changeCount.className = 'change-count';
-            if (plan.actions[i].changes && plan.actions[i].changes.length > 0)
-            {
-                changeCount.innerText = ' ' + plan.actions[i].changes.length + ' change';
-                if (plan.actions[i].changes.length != 1) changeCount.innerText += 's';
-            }
-
-            var summary = document.createElement('div');
-            summary.className = 'summary';
-            summary.onclick = handleSummaryToggle(summary);
-            summary.appendChild(badge);
-            summary.appendChild(id);
-            summary.appendChild(changeCount);
-
-            var changesTable = document.createElement('table');
-            for (var c = 0; c < plan.actions[i].changes.length; c++)
-            {
-                var property = document.createElement('td');
-                property.className = 'property';
-                property.innerText = plan.actions[i].changes[c].property;
-
-                if (plan.actions[i].changes[c].old)
-                {
-                    var oldValue = document.createElement('td');
-                    oldValue.className = 'old-value';
-                    if (plan.actions[i].changes[c].old.indexOf('\\n') >= 0 || plan.actions[i].changes[c].old.indexOf('\\"') >= 0)
-                    {
-                        var pre = document.createElement('pre');
-                        pre.innerHTML = plan.actions[i].changes[c].old;
-                        pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\n', 'g'), '\n');
-                        pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\"', 'g'), '"');
-                        oldValue.appendChild(pre);
-                    }
-                    else
-                    {
-                        oldValue.innerHTML = plan.actions[i].changes[c].old;
-                    }
-                }
-
-                var newValue = document.createElement('td');
-                newValue.className = 'new-value';
-                if (plan.actions[i].changes[c].new.indexOf('\\n') >= 0 || plan.actions[i].changes[c].new.indexOf('\\"') >= 0)
-                {
-                    var pre = document.createElement('pre');
-                    pre.innerHTML = plan.actions[i].changes[c].new;
-                    pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\n', 'g'), '\n');
-                    pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\"', 'g'), '"');
-                    newValue.appendChild(pre);
-                }
-                else
-                {
-                    newValue.innerText = plan.actions[i].changes[c].new;
-                }
-
-                var row = document.createElement('tr');
-                row.appendChild(property);
-                if (plan.actions[i].changes[c].old) row.appendChild(oldValue);
-                row.appendChild(newValue);
-                changesTable.appendChild(row);
-            }
-            
-            var changes = document.createElement('div');
-            changes.className = 'changes collapsed';
-            changes.appendChild(changesTable);
-
-            var listElement = document.createElement('li');
-            listElement.className = plan.actions[i].type;
-            listElement.appendChild(summary);
-            listElement.appendChild(changes);
-            actionList.appendChild(listElement);
+            var action = createActionElement(plan.actions[i]);
+            actionList.appendChild(action);
         }
     }
+}
+
+function createWarningElement(warning) 
+{
+    var badge = createBadgeElement('warning');
+
+    var id = createIdElement(warning.id);
+
+    var detail = document.createElement('span');
+    detail.innerText = warning.detail;
+    
+    var warningElement = document.createElement('li');
+    warningElement.appendChild(badge);
+    warningElement.appendChild(id);
+    warningElement.appendChild(detail);
+    
+    return warningElement;
+}
+
+function createActionElement(action) {
+    var badge = createBadgeElement(action.type);
+    
+    var id = createIdElement(action.id);
+    
+    var changeCount = createChangeCountElement(action.changes ? action.changes.length : 0);
+
+    var heading = createActionSummaryElement(badge, id, changeCount);
+
+    var changesTable = document.createElement('table');
+    for (var i = 0; i < action.changes.length; i++) {
+        var row = createChangeRowElement(action.changes[i]);
+        changesTable.appendChild(row);
+    }
+
+    var changes = document.createElement('div');
+    changes.className = 'changes collapsed';
+    changes.appendChild(changesTable);
+
+    var actionElement = document.createElement('li');
+    actionElement.className = action.type;
+    actionElement.appendChild(heading);
+    actionElement.appendChild(changes);
+    
+    return actionElement;
+}
+
+function createChangeRowElement(change) {
+    var property = document.createElement('td');
+    property.className = 'property';
+    property.innerText = change.property;
+
+    if (change.old) {
+        var oldValue = createChangeRowValueElement('old-value', change.old);
+    }
+
+    var newValue = createChangeRowValueElement('new-value', change.new);
+
+    var row = document.createElement('tr');
+    row.appendChild(property);
+    if (change.old) row.appendChild(oldValue);
+    row.appendChild(newValue);
+    
+    return row;
+}
+
+function createChangeRowValueElement(type, value) {
+    var element = document.createElement('td');
+    element.className = type;
+
+    if (value.indexOf('\\n') >= 0 || value.indexOf('\\"') >= 0) {
+        var pre = document.createElement('pre');
+        pre.innerHTML = value;
+        pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\n', 'g'), '\n');
+        pre.innerHTML = pre.innerHTML.replace(new RegExp('\\\\"', 'g'), '"');
+        element.appendChild(pre);
+    }
+    else {
+        element.innerHTML = value;
+    }
+
+    return element;
+}
+
+function createActionSummaryElement(badge, id, changeCount) {
+    var summary = document.createElement('div');
+    summary.className = 'summary';
+    summary.onclick = handleSummaryToggle(summary);
+    summary.appendChild(badge);
+    summary.appendChild(id);
+    summary.appendChild(changeCount);
+
+    return summary;
+}
+
+function createChangeCountElement(changeCount) {
+    var changeCountElement = document.createElement('span');
+    changeCountElement.className = 'change-count';
+    if (changeCount > 0) {
+        changeCountElement.innerText = ' ' + changeCount + ' change';
+        if (changeCount != 1)
+            changeCountElement.innerText += 's';
+    }
+    return changeCountElement;
+}
+
+function createIdElement(id) {
+    var idElement = document.createElement('span');
+    idElement.className = 'id';
+    idElement.innerText = id;
+    return idElement;
+}
+
+function createBadgeElement(label) {
+    var badge = document.createElement('span');
+    badge.className = 'badge';
+    badge.innerText = label;
+    return badge;
 }
 
 function expandAll()
