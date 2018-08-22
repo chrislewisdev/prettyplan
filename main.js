9bfe40f8f5a4f4b769c2e903678ec36d8e428344
chrisl <chrisl@pageuppeople.com>
2018-08-22 11:51:06 +1000

    Refactored the parse() function into several smaller functions

--- main.js
+++ main.js
@@ -43,81 +43,122 @@ function run()
 
 function parse(terraformPlan)
 {
+    var warnings = parseWarnings(terraformPlan);
+    
+    var changeSummary = extractChangeSummary(terraformPlan);
+    var changes = extractIndividualChanges(changeSummary);
+    
+    var plan = { warnings: warnings, actions: [] };
+    for (var i = 0; i < changes.length; i++)
+    {
+        plan.actions.push(parseChange(changes[i]));
+    }
+    
+    return plan;
+}
+
+function parseWarnings(terraformPlan) {
     var warningRegex = new RegExp('Warning: (.*:)(.*)', 'gm');
     var warning;
     var warnings = [];
-
-    do
-    {
+    
+    do {
         warning = warningRegex.exec(terraformPlan);
-        if (warning) warnings.push({ id: warning[1], detail: warning[2] });
+        if (warning)
+        warnings.push({ id: warning[1], detail: warning[2] });
     } while (warning);
+    
+    return warnings;
+}
 
-    // console.log(warnings);
-
+function extractChangeSummary(terraformPlan) {
     var beginActionRegex = new RegExp('Terraform will perform the following actions:', 'gm');
     var begin = beginActionRegex.exec(terraformPlan);
-    var actions = terraformPlan.substring(begin.index + 45);
+    
+    return terraformPlan.substring(begin.index + 45);
+}
 
-    //TODO: fix chopping off the '-/' in '-/+' changes
+function extractIndividualChanges(changeSummary) {
+    //TODO: Fix thee '-/' in '-/+' getting chopped off
     var changeRegex = new RegExp('([~+-]|-\/\+|<=) [\\S\\s]*?\\s(?=-\/\+|[~+-]|<=|Plan:)', 'gm');
     var change;
     var changes = [];
 
-    // console.log(actions);
-    do
-    {
-        change = changeRegex.exec(actions);
-        // console.log(change);
+    do {
+        change = changeRegex.exec(changeSummary);
         if (change) changes.push(change[0]);
-    } while(change);
+    } while (change);
+
+    return changes;
+}
+
+function parseChange(change) {
+    var changeTypeAndIdRegex = new RegExp('([~+-]|-\/\+|<=) (.*)$', 'gm');
+    var changeTypeAndId = changeTypeAndIdRegex.exec(change);
+    var changeTypeSymbol = changeTypeAndId[1];
+    var resourceId = changeTypeAndId[2];
     
-    // console.log(actions.match(new RegExp('([~+-]|-\/\+) .*?(?=^ {0,2}[~+-]|-\/\+|Plan)', 'gms')));
-    // console.log(changes);
+    var type;
+    type = parseChangeSymbol(changeTypeSymbol, type);
 
-    var summary = { warnings: warnings, actions: [] };
-    for (var i = 0; i < changes.length; i++)
-    {
-        var type;
-        var idRegex = new RegExp('([~+-]|-\/\+|<=) (.*)$', 'gm');
-        var id = idRegex.exec(changes[i]);
-        if (id[1] === "-") type = 'destroy';
-        else if (id[1] === "+") type = 'create';
-        else if (id[1] === "~") type = 'update';
-        else if (id[1] === "<=") type = 'read';
-
-        if (id[2].match('(new resource required)'))
-        {
-            type = 'recreate';
-            id[2] = id[2].replace(' (new resource required)', '');
-        }
-
-        //TODO: This won't pick anything up if it's a CREATE effect
-        //this should work: ^ *(.*?): *"?(.*?)"?$
-        var diffRegex = new RegExp('^ *(.*): *"(.*)" => "?(.*?)"?$', 'gm');
-        var createRegex = new RegExp('^ *(.*?): *"?(.*?)"?$', 'gm');
-        var diff;
-        var diffs = [];
-
-        do
-        {
-            if (type === 'create' || type === 'read')
-            {
-                diff = createRegex.exec(changes[i]);
-                if (diff) diffs.push({ property: diff[1], new: diff[2]});
-            }
-            else
-            {
-                diff = diffRegex.exec(changes[i]);
-                if (diff) diffs.push({ property: diff[1], old: diff[2], new: diff[3] });
-            }
-        } while (diff);
-        
-
-        summary.actions.push({ id: id[2], type: type, changes: diffs });
+    //Workaround for recreations showing up as '+' changes
+    if (resourceId.match('(new resource required)')) {
+        type = 'recreate';
+        resourceId = resourceId.replace(' (new resource required)', '');
     }
 
-    return summary;
+    var diffs;
+    if (type === 'create' || type === 'read') {
+        diffs = parseSingleValueDiffs(change);
+    }
+    else {
+        diffs = parseNewAndOldValueDiffs(change);
+    }
+    
+    return { 
+        id: resourceId, 
+        type: type, 
+        changes: diffs 
+    };
+}
+
+function parseChangeSymbol(changeTypeSymbol, type) {
+    if (changeTypeSymbol === "-")
+        return 'destroy';
+    else if (changeTypeSymbol === "+")
+        return 'create';
+    else if (changeTypeSymbol === "~")
+        return 'update';
+    else if (changeTypeSymbol === "<=")
+        return 'read';
+}
+
+function parseSingleValueDiffs(change)
+{
+    var propertyAndValueRegex = new RegExp('^ *(.*?): *"?(.*?)"?$', 'gm');
+    var diff;
+    var diffs = [];
+
+    do {
+        diff = propertyAndValueRegex.exec(change);
+        if (diff) diffs.push({ property: diff[1], new: diff[2] });
+    } while (diff);
+
+    return diffs;
+}
+
+function parseNewAndOldValueDiffs(change)
+{
+    var propertyAndNewAndOldValueRegex = new RegExp('^ *(.*): *"(.*)" => "?(.*?)"?$', 'gm');
+    var diff;
+    var diffs = [];
+
+    do {
+        diff = propertyAndNewAndOldValueRegex.exec(change);
+        if (diff) diffs.push({ property: diff[1], old: diff[2], new: diff[3] });
+    } while (diff);
+
+    return diffs;
 }
 
 function output(plan)
