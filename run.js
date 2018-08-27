ee0b63655e205e96ef072a0afef42571cdee31de
chrisl <chrisl@pageuppeople.com>
2018-08-27 12:54:18 +1000

    Clear any existing output before rendering out parsed plans

--- run.js
+++ run.js
@@ -1,5 +1,7 @@
 function run()
 {
+    clearExistingPlan();
+    
     var terraformPlan = document.getElementById("terraform-plan").value;
     var plan = parse(terraformPlan);
     console.log(plan);
