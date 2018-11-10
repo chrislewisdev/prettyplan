9e9738d479723b044ca4103e9270aea1f359d098
Chris Lewis <cl_myles@hotmail.com>
2018-11-10 14:59:15 +1100

    New release to signify some of the recent changes

--- js/releases.js
+++ js/releases.js
@@ -13,6 +13,13 @@ function updateLastUsedVersion() {
 //New releases should always go at the top of this list.
 var releases = [
     {
+        version: 'v1.1',
+        notes: [
+            'Added handy release notes!',
+            'Fixed parsing of large AWS IAM policy documents (<a target="_blank" href="https://github.com/chrislewisdev/prettyplan/issues/10">#10</a>)'
+        ]
+    },
+    {
         version: 'v1.0',
         notes: [
             'See your Terraform plans transformed into a beautiful tabulated format!',
