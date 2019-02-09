50a6743183ffb3b9468faffc3a416b3301c01a56
Chris Lewis <cl_myles@hotmail.com>
2019-02-10 00:18:06 +1100

    Added typescript and types to some files

--- src/js/prettyplan.ts
+++ src/js/prettyplan.ts
@@ -1,7 +1,7 @@
 import { getCurrentVersion, getLastUsedVersion, updateLastUsedVersion } from './releases.js';
 import { expandAll, collapseAll, accordion, closeModal } from './ui.js';
 import { showReleaseNotification, hideReleaseNotification, showReleaseNotes, displayParsingErrorMessage, hideParsingErrorMessage, clearExistingOutput, unHidePlan, render } from './render.js';
-import { parse } from './parse.js';
+import { parse } from './parse';
 
 window.addEventListener('load', function () {
     if (getCurrentVersion() != getLastUsedVersion()) {
@@ -10,11 +10,11 @@ window.addEventListener('load', function () {
     }
 }); 
 
-window.runPrettyplan = function() {
+(<any>window).runPrettyplan = () => {
     hideParsingErrorMessage();
     clearExistingOutput();
 
-    var terraformPlan = document.getElementById("terraform-plan").value;
+    var terraformPlan = (<HTMLTextAreaElement>document.getElementById("terraform-plan")).value;
     var plan = parse(terraformPlan);
 
     if (plan.warnings.length === 0 && plan.actions.length === 0) {
@@ -25,9 +25,9 @@ window.runPrettyplan = function() {
     unHidePlan();
 }
 
-window.showReleaseNotes = showReleaseNotes;
-window.expandAll = expandAll;
-window.collapseAll = collapseAll;
-window.accordion = accordion;
-window.closeModal = closeModal;
-window.hideReleaseNotification = hideReleaseNotification;
+(<any>window).showReleaseNotes = showReleaseNotes;
+(<any>window).expandAll = expandAll;
+(<any>window).collapseAll = collapseAll;
+(<any>window).accordion = accordion;
+(<any>window).closeModal = closeModal;
+(<any>window).hideReleaseNotification = hideReleaseNotification;
